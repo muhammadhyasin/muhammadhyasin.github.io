405caafa7dd6d8922aa537dd9770026c70f95e69
muhammadhyasin <muhammadhyaseenka@gmail.com>
2021-04-27 17:56:08 +0530

    Deploy to special repository

---
+++ precache-manifest.38aaa701f26a00e05034cbbe49e486b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a747736f4050928a993769e4eea45ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37349abb7d44024f4308",
+    "url": "/static/css/main.6b6e7380.chunk.css"
+  },
+  {
+    "revision": "be69a7a4b6e1c1e31446",
+    "url": "/static/js/2.97dfb79c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.97dfb79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37349abb7d44024f4308",
+    "url": "/static/js/main.f4bd1c8f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b9773d2494320aead0f6c979e345e824",
+    "url": "/static/media/hashirshoaeb.b9773d24.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
